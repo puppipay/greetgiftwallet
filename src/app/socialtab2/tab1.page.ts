e014a105380645473341a93ac3ca06772a85a108
Ramesh <raga2560@gmail.com>
2019-07-08 18:13:51 +0530

    update

--- src/app/socialtab2/tab1.page.ts
+++ src/app/socialtab2/tab1.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  ElementRef ,ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
+import * as jspdf from 'jspdf'; 
+import html2canvas from 'html2canvas';
 
 
 @Component({
@@ -8,6 +10,9 @@ import { Storage } from '@ionic/storage';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+// https://neelbhatt.com/2018/09/16/convert-html-to-pdf-using-angular-6/
+
 export class Tab1Page implements OnInit{
 
 printingdata: any;
@@ -17,9 +22,27 @@ public whichsegment = "receive";
 
 constructor(
 	private storage: Storage,
-
   ) {
   this.loadprintingdata() ;
+}
+
+
+captureScreen()
+{
+var data = document.getElementById('contentToConvert');
+html2canvas(data).then(canvas => {
+// Few necessary setting options
+var imgWidth = 208;
+var pageHeight = 295;
+var imgHeight = canvas.height * imgWidth / canvas.width;
+var heightLeft = imgHeight;
+ 
+const contentDataURL = canvas.toDataURL('image/png')
+let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+var position = 0;
+pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+pdf.save('MYPdf.pdf'); // Generated PDF
+});
 }
 
 
